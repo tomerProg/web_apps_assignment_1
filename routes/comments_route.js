1d5a36a9168d333033ed8111d360e6ce05bdffad
Tomer Shomron <tomercpc01@gmail.com>
2024-11-25 21:04:39 +0200

    created route for getting all comments

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { getAllComments } from '../controllers/comments_controller.js';
 
 const router = express.Router();
+
+router.get('/', getAllComments);
 
 export default router;
