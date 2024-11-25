63965bed0fb5d6854dd02b606e8599daf05e3c80
hashas3435 <55375789+hashas3435@users.noreply.github.com>
2024-11-25 20:44:56 +0200

    Connect db (#7)
    * add environment variable
    
    * connect db

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import express from 'express'
 import commentsRouter from './routes/comments_route.js'
-
+import mongoose from 'mongoose'
 
 dotenv.config();
 const { PORT: serverPort, DB_CONNECT: dbServer } = process.env;
+
+mongoose.connect(dbServer);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/comments', commentsRouter);
+
 app.listen(serverPort, () => {
     console.log(`server listening on port ${serverPort}`);
 });
-
-app.use('/comments', commentsRouter);
