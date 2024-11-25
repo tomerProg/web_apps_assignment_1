77cb6e3aaa931810990652c09a2b0d578ad2982c
omer hasid <omerhasid11@gmail.com>
2024-11-25 21:16:19 +0200

    mix controllers of get all posts with get post by sender

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -12,15 +12,11 @@ export const createPost = async (req, res) => {
     }
 };
 
-export const getPostBySender = async (req, res) => {
+export const getPosts = async (req, res) => {
     const { sender  } = req.query;
     try {
-        if(sender) {
-            const posts = await postModel.find({ owner: sender });
-            res.send(posts);
-        } else {
-            res.status(StatusCodes.NOT_IMPLEMENTED).send('not implemented get all posts')
-        }
+        const posts = await postModel.find(sender ? { owner: sender } : {});
+        res.send(posts);
     } catch (error) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message);
     }
