4c88e482df987fd8a3ecc3561acaaa641908a85b
omer hasid <omerhasid11@gmail.com>
2024-11-25 20:53:10 +0200

    add route

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { createComment } from '../controllers/comments_controller.js';
 
 const router = express.Router();
+router.post('/', createComment);
 
 export default router;
