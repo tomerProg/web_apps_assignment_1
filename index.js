aecf5f55e63d09fd1dbe4e60eb5872d564c23cf1
Tomer Shomron <tomercpc01@gmail.com>
2024-11-25 20:36:42 +0200

    added posts router to server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import express from 'express'
-import commentsRouter from './routes/comments_route.js'
-
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import commentsRouter from './routes/comments_route.js';
+import postsRouter from './routes/posts_route.js';
 
 dotenv.config();
 const { PORT: serverPort, DB_CONNECT: dbServer } = process.env;
@@ -11,8 +11,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
+
 app.listen(serverPort, () => {
     console.log(`server listening on port ${serverPort}`);
 });
-
-app.use('/comments', commentsRouter);
