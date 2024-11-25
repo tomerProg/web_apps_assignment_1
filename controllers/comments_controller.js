659cb1b1f017aa2223c15f0ce1e2d04f86d770b5
Tomer Shomron <106511342+tomerProg@users.noreply.github.com>
2024-11-25 22:02:45 +0200

    Get all comments (#12)
    * created route for getting all comments
    
    * added request to request.rest

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -2,6 +2,15 @@ import { StatusCodes } from 'http-status-codes';
 import commentModel from '../models/comments_model.js';
 
 
+export const getAllComments = async (_req, res) => {
+    try {
+        const comments = await commentModel.find();
+        res.send(comments);
+    } catch (error) {
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message);
+    }
+};
+
 export const createComment = async (req, res) => {
     const commentBody = req.body;
 
--- routes/comments_route.js
+++ routes/comments_route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createComment, deleteComment, updateComment } from '../controllers/comments_controller.js';
+import { createComment, deleteComment, updateComment, getAllComments } from '../controllers/comments_controller.js';
 
 const router = express.Router();
 
+router.get('/', getAllComments);
 router.post('/', createComment);
 router.put('/:commentId', updateComment);
 router.delete('/:commitId', deleteComment);
