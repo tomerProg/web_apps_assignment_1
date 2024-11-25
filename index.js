ad789be1ba20bf545c5f532239ac5a50096187f6
Tomer Shomron <106511342+tomerProg@users.noreply.github.com>
2024-11-25 20:47:06 +0200

    Create post (#6)
    * created router for creating posts
    
    * add request to request.rest
    
    * added posts router to server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import bodyParser from 'body-parser'
-import dotenv from 'dotenv'
-import express from 'express'
-import commentsRouter from './routes/comments_route.js'
-import mongoose from 'mongoose'
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import commentsRouter from './routes/comments_route.js';
+import postsRouter from './routes/posts_route.js';
 
 dotenv.config();
 const { PORT: serverPort, DB_CONNECT: dbServer } = process.env;
@@ -16,6 +17,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/posts', postsRouter);
 app.use('/comments', commentsRouter);
 
 app.listen(serverPort, () => {
