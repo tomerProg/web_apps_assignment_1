87350ed4daf695345420783e61240cce9b42e2b4
omer hasid <omerhasid11@gmail.com>
2024-11-25 22:10:47 +0200

    add route

--- routes/posts_route.js
+++ routes/posts_route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createPost, getPosts, updatePost, deletePost } from '../controllers/posts_controller.js';
+import { createPost, getPosts, getPostById, updatePost, deletePost } from '../controllers/posts_controller.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/:postId', getPostById);
 router.post('/', createPost);
 router.put('/:postId', updatePost);
 router.delete('/:postId', deletePost);
