7ad9af29cc808f7dbd5d334b38f5a342af87c168
omer hasid <omerhasid11@gmail.com>
2024-11-25 22:18:20 +0200

    modify route implementation

--- routes/comments_route.js
+++ routes/comments_route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { createComment, deleteComment, updateComment, getAllComments } from '../controllers/comments_controller.js';
+import { createComment, deleteComment, updateComment, getComments } from '../controllers/comments_controller.js';
 
 const router = express.Router();
 
-router.get('/', getAllComments);
+router.get('/', getComments);
 router.post('/', createComment);
 router.put('/:commentId', updateComment);
 router.delete('/:commitId', deleteComment);
