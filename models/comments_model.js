e542cc5da6b8afa2a203ea86c10549775d6e218e
hashas3435 <55375789+hashas3435@users.noreply.github.com>
2024-11-25 20:07:22 +0200

    create mongoose model for comments (#3)

---
+++ models/comments_model.js
@@ -0,0 +1,9 @@
+import {Schema, model } from 'mongoose'
+
+const commentSchema = new Schema({
+    user: {type: String, required: true},
+    postId: { type: String, required: true },
+    content: String,
+});
+
+export default model('Comments', commentSchema);
