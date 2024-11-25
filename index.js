e333910c370e491f7777db9d4b5aaf6d5d19a6f6
Tomer Shomron <tomercpc01@gmail.com>
2024-11-25 19:47:09 +0200

    created express app

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('not implemented');
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config();
+const { PORT: serverPort, DB_CONNECT: dbServer } = process.env;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(serverPort, () => {
+    console.log(`server listening on port ${serverPort}`);
+});
