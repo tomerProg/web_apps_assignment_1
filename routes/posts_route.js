9f45dcd57e984e7bd4e89b2e3a85698262f95808
Tomer Shomron <tomercpc01@gmail.com>
2024-11-25 20:54:23 +0200

    created route for getting post by sender

--- routes/posts_route.js
+++ routes/posts_route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createPost } from '../controllers/posts_controller.js';
+import { createPost, getPostBySender } from '../controllers/posts_controller.js';
 
 const router = express.Router();
 
+router.get('/', getPostBySender);
 router.post('/', createPost);
 
 export default router;
