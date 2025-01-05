d0738d15d5324f5bdd6f9a37eb84308bf1415eb0
Tomer Shomron <tomercpc01@gmail.com>
2025-01-06 01:17:15 +0200

    migrate post model to typescript

---
+++ src/models/posts_model.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from 'mongoose';
+
+export interface IPost {
+    title: string;
+    content: string;
+    owner: string;
+  }
+
+const postSchema = new Schema<IPost>({
+    title: { type: String, required: true },
+    content: String,
+    owner: { type: String, required: true },
+});
+
+export default model<IPost>('Posts', postSchema);
