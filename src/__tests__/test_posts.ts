b83d3a47f15c2671c42a4bf0d77384b578c72455
Tomer Shomron <tomercpc01@gmail.com>
2025-01-07 00:51:11 +0200

    tests for posts

---
+++ src/__tests__/test_posts.ts
@@ -0,0 +1,31 @@
+import { IPost } from '../models/posts_model';
+
+export const firstOwner = 'owner1';
+export const firstOwnerPosts: IPost[] = [
+    {
+        title: 'test post 1',
+        content: 'content 1',
+        owner: firstOwner,
+    },
+    {
+        title: 'test post 2',
+        content: 'content 2',
+        owner: firstOwner,
+    },
+];
+
+export const secondOwner = 'owner2';
+export const secondOwnerPosts: IPost[] = [
+    {
+        title: 'test post 3',
+        content: 'content 3',
+        owner: secondOwner,
+    },
+    {
+        title: 'test post 4',
+        content: 'content 4',
+        owner: secondOwner,
+    },
+];
+
+export const allTestPosts = firstOwnerPosts.concat(secondOwnerPosts);
