faef4b921d66b210803a74b68be9cf54c811db5b
omer hasid <omerhasid11@gmail.com>
2025-01-10 23:34:39 +0200

    fix: clean code

--- src/server.ts
+++ src/server.ts
@@ -4,55 +4,18 @@ import authRouter from './routes/auth_route';
 import commentsRouter from './routes/comments_route';
 import postsRouter from './routes/posts_route';
 import swaggerUI from "swagger-ui-express"
-import swaggerJsDoc from "swagger-jsdoc"
-//import specs from './swagger'
+import specs from './swagger'
 
 export const createApp = (): Express => {
     const app = express();
-
-
-    const options = {
-        definition: {
-        openapi: "3.0.0",
-        info: {
-        title: "Web Dev 2022 REST API",
-        version: "1.0.0",
-        description: "REST server including authentication using JWT",
-        },
-        servers: [{url: "http://localhost:3000",},],
-        },
-        apis: ["**/*.ts"],
-    };
-    const specs = swaggerJsDoc(options);
-    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-    
-
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/auth', authRouter);
     app.use('/posts', postsRouter);
     app.use('/comments', commentsRouter);
-
-
     
-    // const options = {
-    //     definition: {
-    //         openapi: "3.0.0",
-    //         info: {
-    //             title: "Web Apps Assignment REST API",
-    //             version: "1.0.0",
-    //             description: "REST server including authentication using JWT",
-    //         },
-    //         servers: [{url: "http://localhost:8080",},],
-    //     },
-    //     apis: ["./src/routes/*.ts"],
-    // };
-    // const specs = swaggerJsDoc(options);
-    // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-       
-
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));    
     return app;
 };
 
--- src/swagger.ts
+++ src/swagger.ts
@@ -8,7 +8,7 @@ const options = {
             version: "1.0.0",
             description: "REST server including authentication using JWT",
         },
-        servers: [{url: "http://localhost:8080",},],
+        servers: [{url: 'http://localhost:3000',},],
     },
     apis: ["./src/routes/*.ts"],
 };
