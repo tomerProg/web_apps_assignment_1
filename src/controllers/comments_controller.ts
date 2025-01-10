0201bf5e9a1d2be412cafa2537191dd905a978b8
omer hasid <omerhasid11@gmail.com>
2025-01-10 17:40:43 +0200

    add getByFilter to baseController

--- src/controllers/comments_controller.ts
+++ src/controllers/comments_controller.ts
@@ -1,6 +1,16 @@
+import { Request, Response } from 'express';
 import commentModel, {IComment} from '../models/comments_model';
 import BaseController from "./base_controller";
+class commentsController extends BaseController<IComment>{
+    constructor(){
+        super(commentModel)
+    }
+    
+    async getByPostId(req: Request, res: Response) {
+        const filter = req.query.postId ? {postId: req.query.postId} : {};
+        return this.getByFilter(req, res, filter);
+    }
+}
 
-const commentsController = new BaseController<IComment>(commentModel);
 
 export default commentsController
