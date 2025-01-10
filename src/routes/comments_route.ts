35424956a7180f882544fccb1b20585d7309df2e
Tomer Shomron <tomercpc01@gmail.com>
2025-01-11 00:33:37 +0200

    added auth middleware to comments routes

--- src/routes/comments_route.ts
+++ src/routes/comments_route.ts
@@ -1,12 +1,13 @@
 import {Router} from 'express';
 import CommentController from '../controllers/comments_controller';
+import { authMiddleware } from '../controllers/auth_controller';
 
 const router = Router();
 
 router.get('/', CommentController.getAll.bind(CommentController));
 router.get('/:id', CommentController.getById.bind(CommentController));
-router.post('/', CommentController.create.bind(CommentController));
-router.put('/:id', CommentController.update.bind(CommentController));
-router.delete('/:id', CommentController.deleteItem.bind(CommentController));
+router.post('/', authMiddleware, CommentController.create.bind(CommentController));
+router.put('/:id', authMiddleware, CommentController.update.bind(CommentController));
+router.delete('/:id', authMiddleware, CommentController.deleteItem.bind(CommentController));
 
 export default router;
