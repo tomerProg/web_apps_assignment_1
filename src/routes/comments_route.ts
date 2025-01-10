14ec43b5dd996ff6831a75a6041c4c9fbaa94c98
omer hasid <omerhasid11@gmail.com>
2025-01-10 18:38:58 +0200

    fix: comment post delete prefix

--- src/routes/comments_route.ts
+++ src/routes/comments_route.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get('/', CommentController.getAll.bind(CommentController));
 router.get('/:id', CommentController.getById.bind(CommentController));
 router.post('/', CommentController.create.bind(CommentController));
-router.put('/:commentId', CommentController.update.bind(CommentController));
-router.delete('/:commitId', CommentController.deleteItem.bind(CommentController));
+router.put('/:id', CommentController.update.bind(CommentController));
+router.delete('/:id', CommentController.deleteItem.bind(CommentController));
 
 export default router;
