c4c73283bc48f61b40ebfbbb1df7bc1bff53a5dc
Tomer Shomron <106511342+tomerProg@users.noreply.github.com>
2025-01-11 13:09:00 +0200

    44 auth for comment routes (#45)
    * added auth middleware to comments routes
    
    * fixed tests to work with authentication

--- src/__tests__/comments.test.ts
+++ src/__tests__/comments.test.ts
@@ -1,24 +1,40 @@
 import { StatusCodes } from 'http-status-codes';
+import { Types as MongooseTypes } from 'mongoose';
 import request from 'supertest';
 import startDatabase, { Database } from '../database';
-import CommentModel, { IComment } from '../models/comments_model';
+import CommentModel from '../models/comments_model';
+import usersModel, { IUser } from '../models/users_model';
 import { createApp } from '../server';
-import { allTestComments, firstOwnerComments, firstOwner, secondOwnerComments } from './test_comments'
+import { createUserAuthenticationToken } from './auth.test';
+import {
+    allTestComments,
+    firstOwner,
+    firstOwnerComments,
+    secondOwnerComments
+} from './test_comments';
 
 describe('Comments Router', () => {
     let database: Database;
     const app = createApp();
 
+    const user: IUser = {
+        email: 'userEmail',
+        password: '123456'
+    };
+    let authenticationToken: string;
+
     beforeAll(async () => {
         database = await startDatabase();
         await CommentModel.deleteMany();
+        authenticationToken = await createUserAuthenticationToken(app, user);
     });
     afterAll(async () => {
+        await usersModel.deleteMany();
         await database.disconnect();
     });
 
     beforeEach(async () => {
-        await CommentModel.insertMany(allTestComments)
+        await CommentModel.insertMany(allTestComments);
     });
     afterEach(async () => {
         await CommentModel.deleteMany();
@@ -30,26 +46,40 @@ describe('Comments Router', () => {
 
             expect(response.status).toBe(StatusCodes.OK);
             expect(response.body).toStrictEqual(
-                expect.arrayContaining(allTestComments.map((comment) => expect.objectContaining(comment)))
+                expect.arrayContaining(
+                    allTestComments.map((comment) =>
+                        expect.objectContaining(comment)
+                    )
+                )
             );
         });
 
         test('get all comments with owner should return owner comments', async () => {
-            const response = await request(app).get('/comments').query({ owner: firstOwner });
+            const response = await request(app)
+                .get('/comments')
+                .query({ owner: firstOwner });
 
             expect(response.status).toBe(StatusCodes.OK);
-            expect(response.body).toStrictEqual(firstOwnerComments.map((comment) => expect.objectContaining(comment)));
+            expect(response.body).toStrictEqual(
+                firstOwnerComments.map((comment) =>
+                    expect.objectContaining(comment)
+                )
+            );
             secondOwnerComments.forEach((comment) => {
-                expect(response.body).not.toContainEqual(expect.objectContaining(comment));
+                expect(response.body).not.toContainEqual(
+                    expect.objectContaining(comment)
+                );
             });
         });
 
         test('not existing comment should return NOT_FOUND', async () => {
-            const response = await request(app).get('/comments/000000000000000000000000');
+            const response = await request(app).get(
+                '/comments/000000000000000000000000'
+            );
 
             expect(response.status).toBe(StatusCodes.NOT_FOUND);
         });
-        
+
         test('id not in ObjectId format should return BAD_REQUEST', async () => {
             const response = await request(app).get('/comments/randomId');
 
@@ -58,10 +88,17 @@ describe('Comments Router', () => {
     });
 
     describe('delete comment by id', () => {
+        const requestDeleteComment = (_id: string | MongooseTypes.ObjectId) =>
+            request(app)
+                .delete(`/comments/${_id}`)
+                .set({ authorization: 'JWT ' + authenticationToken });
+
         test('existing comment should return the comment and delete it', async () => {
             const expectedComment = allTestComments[1];
-            const { _id } = (await CommentModel.findOne(expectedComment).lean())!;
-            const response = await request(app).delete(`/comments/${_id}`);
+            const { _id } = (await CommentModel.findOne(
+                expectedComment
+            ).lean())!;
+            const response = await requestDeleteComment(_id);
             const doc = await CommentModel.findById(_id);
 
             expect(response.status).toBe(StatusCodes.OK);
@@ -69,66 +106,85 @@ describe('Comments Router', () => {
         });
 
         test('id not in ObjectId format should return BAD_REQUEST', async () => {
-            const response = await request(app).delete('/comments/randomId');
+            const response = await requestDeleteComment('randomId');
 
             expect(response.status).toBe(StatusCodes.BAD_REQUEST);
         });
 
         test('not existing comment should return OK', async () => {
-            const response = await request(app).delete('/comments/000000000000000000000000');
+            const response = await requestDeleteComment(
+                '000000000000000000000000'
+            );
 
             expect(response.status).toBe(StatusCodes.OK);
         });
     });
-    
+
     describe('update comment by id', () => {
+        const requestUpdateComment = (_id: string | MongooseTypes.ObjectId) =>
+            request(app)
+                .put(`/comments/${_id}`)
+                .set({ authorization: 'JWT ' + authenticationToken });
+
         test('valid update should update the comment', async () => {
             const expectedComment = allTestComments[1];
-            const { _id } = (await CommentModel.findOne(expectedComment).lean())!;
+            const { _id } = (await CommentModel.findOne(
+                expectedComment
+            ).lean())!;
             const update = { content: 'updated content' };
-            const response = await request(app).put(`/comments/${_id}`).send(update);
+            const response = await requestUpdateComment(_id).send(update);
             const updatedComment = await CommentModel.findById(_id);
 
             expect(response.status).toBe(StatusCodes.OK);
             expect(updatedComment).toBeDefined();
-            expect(updatedComment).toStrictEqual(expect.objectContaining(update));
+            expect(updatedComment).toStrictEqual(
+                expect.objectContaining(update)
+            );
         });
 
         test('id not in ObjectId format should return INTERNAL_SERVER_ERROR status', async () => {
-            const response = await request(app).put('/comments/randomId');
+            const response = await requestUpdateComment('randomId');
 
             expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
         });
 
         test('invalid update field should return INTERNAL_SERVER_ERROR status', async () => {
             const expectedComment = allTestComments[1];
-            const { _id } = (await CommentModel.findOne(expectedComment).lean())!;
+            const { _id } = (await CommentModel.findOne(
+                expectedComment
+            ).lean())!;
             const update = { content: { field: 'field' } };
-            const response = await request(app).put(`/comments/${_id}`).send(update);
+            const response = await requestUpdateComment(_id).send(update);
 
             expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
         });
     });
 
     describe('create comment', () => {
+        const requestUpdateComment = () =>
+            request(app)
+                .post('/comments')
+                .set({ authorization: 'JWT ' + authenticationToken });
+
         test('valid new comment should create new comment', async () => {
-            const newComment: IComment = {
+            const newComment = {
                 content: 'content',
-                owner: 'new owner',
                 postId: 'new post'
             };
-            const response = await request(app).post('/comments').send(newComment);
+            const response = await requestUpdateComment().send(newComment);
 
             expect(response.status).toBe(StatusCodes.CREATED);
-            expect(response.body).toStrictEqual(expect.objectContaining(newComment));
+            expect(response.body).toStrictEqual(
+                expect.objectContaining(newComment)
+            );
         });
 
         test('missing comment field should return BAD_REQUEST', async () => {
             const newComment = {
                 content: 'content',
-                owner: 'new owner',
+                owner: 'new owner'
             };
-            const response = await request(app).post('/comments').send(newComment);
+            const response = await requestUpdateComment().send(newComment);
 
             expect(response.status).toBe(StatusCodes.BAD_REQUEST);
         });
--- src/controllers/comments_controller.ts
+++ src/controllers/comments_controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
-import commentModel, {IComment} from '../models/comments_model';
-import BaseController from "./base_controller";
+import commentModel, { IComment } from '../models/comments_model';
+import BaseController from './base_controller';
 
-class CommentController extends BaseController<IComment>{
-    constructor(){
-        super(commentModel)
+class CommentController extends BaseController<IComment> {
+    constructor() {
+        super(commentModel);
+    }
+
+    create(req: Request, res: Response) {
+        const userId = req.params.userId;
+        req.body = {
+            ...req.body,
+            owner: userId
+        };
+
+        return super.create(req, res);
     }
 }
 
